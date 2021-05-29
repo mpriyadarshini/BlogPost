8880cab6f7670a6887ed1ac6ea6357eebb505fe3
Megha Priyadarshini <meghapriyadarshini68@gmail.com>
2021-05-30 01:13:22 +0530

    Added Spring Security

---
+++ src/main/java/com/example/blog/WebSecurityConfig.java
@@ -0,0 +1,60 @@
+package com.example.blog;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+
+@Configuration
+@EnableWebSecurity // to enable spring security's web security support
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception { // the configure  method defines which all URL paths should be secured
+        http
+                .httpBasic()// to enable basic authentication
+                .and()
+                .authorizeRequests()   // checks for authentication
+                .anyRequest()              // matches all requests
+                .authenticated()          // makes sure that they are authenticated
+                .and()
+                .logout()
+                .permitAll();                // allow anyone to logout
+    }
+
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("user")
+//                .password(passwordEncoder().encode("password"))
+//                .roles("USER");
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() { // here userDetailsService sets up an in-memory user storage with a single user.
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+
+}
